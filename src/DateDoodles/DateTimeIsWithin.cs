fdb114255594730bb3b29e35b0b388c4f42c60c3
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-10-03 23:13:06 -0400

    add unit tests for "date time is within"

--- src/DateDoodles/DateTimeIsWithin.cs
+++ src/DateDoodles/DateTimeIsWithin.cs
@@ -11,24 +11,24 @@ public static class IDateTimeWithinExtensions
     public static bool SecondsOf(this IDateTimeIsWithin within, DateTime dateTime)
     {
         var totalSeconds = Math.Abs((within.Is.Value - dateTime).TotalSeconds);
-        return totalSeconds >= within.Number;
+        return totalSeconds <= within.Number;
     }
 
     public static bool MinutesOf(this IDateTimeIsWithin within, DateTime dateTime)
     {
         var totalMinutes = Math.Abs((within.Is.Value - dateTime).TotalMinutes);
-        return totalMinutes >= within.Number;
+        return totalMinutes <= within.Number;
     }
 
     public static bool HoursOf(this IDateTimeIsWithin within, DateTime dateTime)
     {
         var totalHours = Math.Abs((within.Is.Value - dateTime).TotalHours);
-        return totalHours >= within.Number;
+        return totalHours <= within.Number;
     }
 
     public static bool DaysOf(this IDateTimeIsWithin within, DateTime dateTime)
     {
         var totalDays = Math.Abs((within.Is.Value - dateTime).TotalDays);
-        return totalDays >= within.Number;
+        return totalDays <= within.Number;
     }
 }
---
+++ tests/DateDoodles.UnitTests/DateTimeIsWithinTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateDoodles.UnitTests;
+
+public class DateTimeIsWithinTests
+{
+    public static readonly DateTime Now = DateTime.UtcNow;
+
+    [Fact]
+    public void SecondsOf_ReturnsExpected()
+    {
+        var futureValue = Now.AddSeconds(2.5);
+        var pastValue = Now.AddSeconds(-2.5);
+
+        Now.Is().Within(1).SecondsOf(futureValue).Should().BeFalse();
+        Now.Is().Within(2.5).SecondsOf(futureValue).Should().BeTrue();
+        Now.Is().Within(3).SecondsOf(futureValue).Should().BeTrue();
+        Now.Is().Within(3).SecondsOf(pastValue).Should().BeTrue();
+        Now.Is().Within(2.5).SecondsOf(pastValue).Should().BeTrue();
+        Now.Is().Within(1).SecondsOf(pastValue).Should().BeFalse();
+    }
+
+    [Fact]
+    public void MinutesOf_ReturnsExpected()
+    {
+        var futureValue = Now.AddMinutes(2.5);
+        var pastValue = Now.AddMinutes(-2.5);
+
+        Now.Is().Within(1).MinutesOf(futureValue).Should().BeFalse();
+        Now.Is().Within(2.5).MinutesOf(futureValue).Should().BeTrue();
+        Now.Is().Within(3).MinutesOf(futureValue).Should().BeTrue();
+        Now.Is().Within(3).MinutesOf(pastValue).Should().BeTrue();
+        Now.Is().Within(2.5).MinutesOf(pastValue).Should().BeTrue();
+        Now.Is().Within(1).MinutesOf(pastValue).Should().BeFalse();
+    }
+
+    [Fact]
+    public void HoursOf_ReturnsExpected()
+    {
+        var futureValue = Now.AddHours(2.5);
+        var pastValue = Now.AddHours(-2.5);
+
+        Now.Is().Within(1).HoursOf(futureValue).Should().BeFalse();
+        Now.Is().Within(2.5).HoursOf(futureValue).Should().BeTrue();
+        Now.Is().Within(3).HoursOf(futureValue).Should().BeTrue();
+        Now.Is().Within(3).HoursOf(pastValue).Should().BeTrue();
+        Now.Is().Within(2.5).HoursOf(pastValue).Should().BeTrue();
+        Now.Is().Within(1).HoursOf(pastValue).Should().BeFalse();
+    }
+
+
+    [Fact]
+    public void DaysOf_ReturnsExpected()
+    {
+        var futureValue = Now.AddDays(2.5);
+        var pastValue = Now.AddDays(-2.5);
+
+        Now.Is().Within(1).DaysOf(futureValue).Should().BeFalse();
+        Now.Is().Within(2.5).DaysOf(futureValue).Should().BeTrue();
+        Now.Is().Within(3).DaysOf(futureValue).Should().BeTrue();
+        Now.Is().Within(3).DaysOf(pastValue).Should().BeTrue();
+        Now.Is().Within(2.5).DaysOf(pastValue).Should().BeTrue();
+        Now.Is().Within(1).DaysOf(pastValue).Should().BeFalse();
+    }
+}
