2ebabdba9dc9914e2627291ebc82a30be7990ab4
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-10-03 23:29:29 -0400

    add before and after methods

--- src/FluentTime/IDateTimeIs.cs
+++ src/FluentTime/IDateTimeIs.cs
@@ -9,9 +9,19 @@ public interface IDateTimeIs
 
 public static class IDateTimeIsExtensions
 {
+    public static bool After(this IDateTimeIs dateTimeIs, DateTime dateTime)
+    {
+        return dateTimeIs.Value > dateTime;
+    }
+
     public static IDateTimeIsAtLeast AtLeast(this IDateTimeIs dateTimeIs, double number)
     {
         return new DateTimeIsAtLeast(dateTimeIs, number);
+    }
+
+    public static bool Before(this IDateTimeIs dateTimeIs, DateTime dateTime)
+    {
+        return dateTimeIs.Value < dateTime;
     }
 
     public static IDateTimeIsWithin Within(this IDateTimeIs dateTimeIs, double number)
---
+++ tests/FluentTime.UnitTests/DateTiimeIsTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+
+namespace FluentTime.UnitTests;
+
+public class DateTiimeIsTests
+{
+    public static readonly DateTime Now = DateTime.UtcNow;
+
+    [Fact]
+    public void After_ReturnsExpected()
+    {
+        var futureValue = Now.AddSeconds(2.5);
+
+        Now.Is().After(futureValue).Should().BeFalse();
+        Now.Is().After(Now).Should().BeFalse();
+        futureValue.Is().After(Now).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Before_ReturnsExpected()
+    {
+        var futureValue = Now.AddSeconds(2.5);
+
+        Now.Is().Before(futureValue).Should().BeTrue();
+        Now.Is().Before(Now).Should().BeFalse();
+        futureValue.Is().Before(Now).Should().BeFalse();
+    }
+}
