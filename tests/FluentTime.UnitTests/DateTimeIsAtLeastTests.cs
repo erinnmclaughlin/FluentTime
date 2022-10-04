e9a990cb77b899637a76985bfdcca075f1e3b115
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-10-04 06:30:47 -0400

    renaming things

--- tests/FluentTime.UnitTests/DateTimeIsAtLeastTests.cs
+++ tests/FluentTime.UnitTests/DateTimeIsAtLeastTests.cs
@@ -4,88 +4,88 @@ namespace FluentTime.UnitTests;
 
 public class DateTimeIsAtLeastTests
 {
-    public static readonly DateTime Now = DateTime.UtcNow;
+    public static readonly DateTime ThePresent = DateTime.UtcNow;
 
     [Fact]
     public void SecondsAfter_ShouldReturnExpected()
     {
-        var futureValue = Now.AddSeconds(2.5);
+        var futureValue = ThePresent.AddSeconds(2.5);
 
-        futureValue.Is().AtLeast(2).Seconds().After(Now).Should().BeTrue();
-        futureValue.Is().AtLeast(2.5).Seconds().After(Now).Should().BeTrue();
-        futureValue.Is().AtLeast(3).Seconds().After(Now).Should().BeFalse();
+        futureValue.Is().AtLeast(2).Seconds().After(ThePresent).Should().BeTrue();
+        futureValue.Is().AtLeast(2.5).Seconds().After(ThePresent).Should().BeTrue();
+        futureValue.Is().AtLeast(3).Seconds().After(ThePresent).Should().BeFalse();
     }
 
     [Fact]
     public void SecondsBefore_ShouldReturnExpected()
     {
-        var futureValue = Now.AddSeconds(2.5);
+        var futureValue = ThePresent.AddSeconds(2.5);
 
-        Now.Is().AtLeast(2).Seconds().Before(futureValue).Should().BeFalse();
-        Now.Is().AtLeast(2.5).Seconds().Before(futureValue).Should().BeTrue();
-        Now.Is().AtLeast(3).Seconds().Before(futureValue).Should().BeTrue();
+        ThePresent.Is().AtLeast(2).Seconds().Before(futureValue).Should().BeFalse();
+        ThePresent.Is().AtLeast(2.5).Seconds().Before(futureValue).Should().BeTrue();
+        ThePresent.Is().AtLeast(3).Seconds().Before(futureValue).Should().BeTrue();
     }
 
     [Fact]
     public void MinutesAfter_ShouldReturnExpected()
     {
-        var futureValue = Now.AddMinutes(2.5);
+        var futureValue = ThePresent.AddMinutes(2.5);
 
-        futureValue.Is().AtLeast(2).Minutes().After(Now).Should().BeTrue();
-        futureValue.Is().AtLeast(2.5).Minutes().After(Now).Should().BeTrue();
-        futureValue.Is().AtLeast(3).Minutes().After(Now).Should().BeFalse();
+        futureValue.Is().AtLeast(2).Minutes().After(ThePresent).Should().BeTrue();
+        futureValue.Is().AtLeast(2.5).Minutes().After(ThePresent).Should().BeTrue();
+        futureValue.Is().AtLeast(3).Minutes().After(ThePresent).Should().BeFalse();
     }
 
     [Fact]
     public void MinutesBefore_ShouldReturnExpected()
     {
-        var futureValue = Now.AddMinutes(2.5);
+        var futureValue = ThePresent.AddMinutes(2.5);
 
-        Now.Is().AtLeast(2).Minutes().Before(futureValue).Should().BeFalse();
-        Now.Is().AtLeast(2.5).Minutes().Before(futureValue).Should().BeTrue();
-        Now.Is().AtLeast(3).Minutes().Before(futureValue).Should().BeTrue();
+        ThePresent.Is().AtLeast(2).Minutes().Before(futureValue).Should().BeFalse();
+        ThePresent.Is().AtLeast(2.5).Minutes().Before(futureValue).Should().BeTrue();
+        ThePresent.Is().AtLeast(3).Minutes().Before(futureValue).Should().BeTrue();
     }
 
     [Fact]
     public void HoursAfter_ShouldReturnExpected()
     {
-        var futureValue = Now.AddHours(2.5);
+        var futureValue = ThePresent.AddHours(2.5);
 
-        futureValue.Is().AtLeast(2).Hours().After(Now).Should().BeTrue();
-        futureValue.Is().AtLeast(2.5).Hours().After(Now).Should().BeTrue();
-        futureValue.Is().AtLeast(3).Hours().After(Now).Should().BeFalse();
+        futureValue.Is().AtLeast(2).Hours().After(ThePresent).Should().BeTrue();
+        futureValue.Is().AtLeast(2.5).Hours().After(ThePresent).Should().BeTrue();
+        futureValue.Is().AtLeast(3).Hours().After(ThePresent).Should().BeFalse();
     }
 
 
     [Fact]
     public void HoursBefore_ShouldReturnExpected()
     {
-        var futureValue = Now.AddHours(2.5);
+        var futureValue = ThePresent.AddHours(2.5);
 
-        Now.Is().AtLeast(2).Hours().Before(futureValue).Should().BeFalse();
-        Now.Is().AtLeast(2.5).Hours().Before(futureValue).Should().BeTrue();
-        Now.Is().AtLeast(3).Hours().Before(futureValue).Should().BeTrue();
+        ThePresent.Is().AtLeast(2).Hours().Before(futureValue).Should().BeFalse();
+        ThePresent.Is().AtLeast(2.5).Hours().Before(futureValue).Should().BeTrue();
+        ThePresent.Is().AtLeast(3).Hours().Before(futureValue).Should().BeTrue();
     }
 
 
     [Fact]
     public void DaysAfter_ShouldReturnExpected()
     {
-        var futureValue = Now.AddDays(2.5);
+        var futureValue = ThePresent.AddDays(2.5);
 
-        futureValue.Is().AtLeast(2).Days().After(Now).Should().BeTrue();
-        futureValue.Is().AtLeast(2.5).Days().After(Now).Should().BeTrue();
-        futureValue.Is().AtLeast(3).Days().After(Now).Should().BeFalse();
+        futureValue.Is().AtLeast(2).Days().After(ThePresent).Should().BeTrue();
+        futureValue.Is().AtLeast(2.5).Days().After(ThePresent).Should().BeTrue();
+        futureValue.Is().AtLeast(3).Days().After(ThePresent).Should().BeFalse();
     }
 
 
     [Fact]
     public void DaysBefore_ShouldReturnExpected()
     {
-        var futureValue = Now.AddDays(2.5);
+        var futureValue = ThePresent.AddDays(2.5);
 
-        Now.Is().AtLeast(2).Days().Before(futureValue).Should().BeFalse();
-        Now.Is().AtLeast(2.5).Days().Before(futureValue).Should().BeTrue();
-        Now.Is().AtLeast(3).Days().Before(futureValue).Should().BeTrue();
+        ThePresent.Is().AtLeast(2).Days().Before(futureValue).Should().BeFalse();
+        ThePresent.Is().AtLeast(2.5).Days().Before(futureValue).Should().BeTrue();
+        ThePresent.Is().AtLeast(3).Days().Before(futureValue).Should().BeTrue();
     }
 }
--- tests/FluentTime.UnitTests/DateTimeIsWithinTests.cs
+++ tests/FluentTime.UnitTests/DateTimeIsWithinTests.cs
@@ -4,62 +4,62 @@ namespace FluentTime.UnitTests;
 
 public class DateTimeIsWithinTests
 {
-    public static readonly DateTime Now = DateTime.UtcNow;
+    public static readonly DateTime ThePresent = DateTime.UtcNow;
 
     [Fact]
     public void SecondsOf_ReturnsExpected()
     {
-        var futureValue = Now.AddSeconds(2.5);
-        var pastValue = Now.AddSeconds(-2.5);
+        var futureValue = ThePresent.AddSeconds(2.5);
+        var pastValue = ThePresent.AddSeconds(-2.5);
 
-        Now.Is().Within(1).Seconds().Of(futureValue).Should().BeFalse();
-        Now.Is().Within(2.5).Seconds().Of(futureValue).Should().BeTrue();
-        Now.Is().Within(3).Seconds().Of(futureValue).Should().BeTrue();
-        Now.Is().Within(3).Seconds().Of(pastValue).Should().BeTrue();
-        Now.Is().Within(2.5).Seconds().Of(pastValue).Should().BeTrue();
-        Now.Is().Within(1).Seconds().Of(pastValue).Should().BeFalse();
+        ThePresent.Is().Within(1).Seconds().Of(futureValue).Should().BeFalse();
+        ThePresent.Is().Within(2.5).Seconds().Of(futureValue).Should().BeTrue();
+        ThePresent.Is().Within(3).Seconds().Of(futureValue).Should().BeTrue();
+        ThePresent.Is().Within(3).Seconds().Of(pastValue).Should().BeTrue();
+        ThePresent.Is().Within(2.5).Seconds().Of(pastValue).Should().BeTrue();
+        ThePresent.Is().Within(1).Seconds().Of(pastValue).Should().BeFalse();
     }
 
     [Fact]
     public void MinutesOf_ReturnsExpected()
     {
-        var futureValue = Now.AddMinutes(2.5);
-        var pastValue = Now.AddMinutes(-2.5);
+        var futureValue = ThePresent.AddMinutes(2.5);
+        var pastValue = ThePresent.AddMinutes(-2.5);
 
-        Now.Is().Within(1).Minutes().Of(futureValue).Should().BeFalse();
-        Now.Is().Within(2.5).Minutes().Of(futureValue).Should().BeTrue();
-        Now.Is().Within(3).Minutes().Of(futureValue).Should().BeTrue();
-        Now.Is().Within(3).Minutes().Of(pastValue).Should().BeTrue();
-        Now.Is().Within(2.5).Minutes().Of(pastValue).Should().BeTrue();
-        Now.Is().Within(1).Minutes().Of(pastValue).Should().BeFalse();
+        ThePresent.Is().Within(1).Minutes().Of(futureValue).Should().BeFalse();
+        ThePresent.Is().Within(2.5).Minutes().Of(futureValue).Should().BeTrue();
+        ThePresent.Is().Within(3).Minutes().Of(futureValue).Should().BeTrue();
+        ThePresent.Is().Within(3).Minutes().Of(pastValue).Should().BeTrue();
+        ThePresent.Is().Within(2.5).Minutes().Of(pastValue).Should().BeTrue();
+        ThePresent.Is().Within(1).Minutes().Of(pastValue).Should().BeFalse();
     }
 
     [Fact]
     public void HoursOf_ReturnsExpected()
     {
-        var futureValue = Now.AddHours(2.5);
-        var pastValue = Now.AddHours(-2.5);
+        var futureValue = ThePresent.AddHours(2.5);
+        var pastValue = ThePresent.AddHours(-2.5);
 
-        Now.Is().Within(1).Hours().Of(futureValue).Should().BeFalse();
-        Now.Is().Within(2.5).Hours().Of(futureValue).Should().BeTrue();
-        Now.Is().Within(3).Hours().Of(futureValue).Should().BeTrue();
-        Now.Is().Within(3).Hours().Of(pastValue).Should().BeTrue();
-        Now.Is().Within(2.5).Hours().Of(pastValue).Should().BeTrue();
-        Now.Is().Within(1).Hours().Of(pastValue).Should().BeFalse();
+        ThePresent.Is().Within(1).Hours().Of(futureValue).Should().BeFalse();
+        ThePresent.Is().Within(2.5).Hours().Of(futureValue).Should().BeTrue();
+        ThePresent.Is().Within(3).Hours().Of(futureValue).Should().BeTrue();
+        ThePresent.Is().Within(3).Hours().Of(pastValue).Should().BeTrue();
+        ThePresent.Is().Within(2.5).Hours().Of(pastValue).Should().BeTrue();
+        ThePresent.Is().Within(1).Hours().Of(pastValue).Should().BeFalse();
     }
 
 
     [Fact]
     public void DaysOf_ReturnsExpected()
     {
-        var futureValue = Now.AddDays(2.5);
-        var pastValue = Now.AddDays(-2.5);
+        var futureValue = ThePresent.AddDays(2.5);
+        var pastValue = ThePresent.AddDays(-2.5);
 
-        Now.Is().Within(1).Days().Of(futureValue).Should().BeFalse();
-        Now.Is().Within(2.5).Days().Of(futureValue).Should().BeTrue();
-        Now.Is().Within(3).Days().Of(futureValue).Should().BeTrue();
-        Now.Is().Within(3).Days().Of(pastValue).Should().BeTrue();
-        Now.Is().Within(2.5).Days().Of(pastValue).Should().BeTrue();
-        Now.Is().Within(1).Days().Of(pastValue).Should().BeFalse();
+        ThePresent.Is().Within(1).Days().Of(futureValue).Should().BeFalse();
+        ThePresent.Is().Within(2.5).Days().Of(futureValue).Should().BeTrue();
+        ThePresent.Is().Within(3).Days().Of(futureValue).Should().BeTrue();
+        ThePresent.Is().Within(3).Days().Of(pastValue).Should().BeTrue();
+        ThePresent.Is().Within(2.5).Days().Of(pastValue).Should().BeTrue();
+        ThePresent.Is().Within(1).Days().Of(pastValue).Should().BeFalse();
     }
 }
