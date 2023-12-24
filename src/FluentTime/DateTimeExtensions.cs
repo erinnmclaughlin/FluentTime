efd8bce54a8cd615e68a4119febe0c958a2476c4
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-12-24 03:04:21 -0500

    add "IsOnOrBefore/After" methods

--- src/FluentTime/DateTimeExtensions.cs
+++ src/FluentTime/DateTimeExtensions.cs
@@ -10,14 +10,24 @@ public static class DateTimeExtensions
     public static bool IsInThePast(this DateTime dt) => Currently.It.IsAfter(dt);
     public static bool IsInTheFuture(this DateTime dt) => Currently.It.IsBefore(dt);
 
-    public static bool IsAfter(this DateTime root, DateTime dateTime)
+    public static bool IsAfter(this DateTime root, DateTime other)
     {
-        return root > dateTime;
+        return root > other;
     }
 
-    public static bool IsBefore(this DateTime root, DateTime dateTime)
+    public static bool IsOnOrAfter(this DateTime root, DateTime other)
     {
-        return root < dateTime;
+        return root >= other;
+    }
+
+    public static bool IsBefore(this DateTime root, DateTime other)
+    {
+        return root < other;
+    }
+
+    public static bool IsOnOrBefore(this DateTime root, DateTime other)
+    {
+        return root <= other;
     }
 
     public static DateTimeIsAtLeastExpressionRoot IsAtLeast(this DateTime root, double number)
